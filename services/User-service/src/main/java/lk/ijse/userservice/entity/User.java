6c4c0046129389c5ca58380d86acf43f76997be2
savindunawanjana <savindunawanjana08@gmail.com>
2026-08-04 07:52:58 +0530

    update files

--- services/User-service/src/main/java/lk/ijse/userservice/entity/User.java
+++ services/User-service/src/main/java/lk/ijse/userservice/entity/User.java
@@ -1,6 +1,8 @@
 package lk.ijse.userservice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lk.ijse.userservice.dto.Enum.Role;
 import lombok.AllArgsConstructor;
@@ -23,6 +25,8 @@ public class User {
     private String name;
     private String password;
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     private String status;
