76baea1c1bba607946def1e2aa4474c70cd57876
savindunawanjana <savindunawanjana08@gmail.com>
2026-08-06 04:46:39 +0530

    updated files and add new files..

---
+++ services/paymen-service/src/test/java/lk/ijse/paymenservice/PaymenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.paymenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
