3af7ebb9f99961218905ea868ec08869010ac395
savindunawanjana <savindunawanjana08@gmail.com>
2026-08-06 06:20:41 +0530

    updated files and  add new files..

---
+++ services/parking-space-sevice/src/test/java/lk/ijse/parkingspacesevice/ParkingSpaceSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.parkingspacesevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpaceSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
